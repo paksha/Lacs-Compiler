abf7b64da053aa69c6c750bde757a73cd65e5484
Akshay Patel <akshaypatel07@hotmail.com>
2021-03-14 10:18:39 -0400

    Add files via upload

---
+++ lacs.scala
@@ -0,0 +1,6 @@
+object Lacs {
+    def main(args: Array[String]) = {
+        println("Hello! Please email me at ab7patel@uwaterloo.ca for the source code.");
+        println("In your email, explain who you are and why you'd like to see my implementation of the Lacs Compiler.")
+    }
+}
